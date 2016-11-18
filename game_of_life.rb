e0ca372919b3a482777e5b4d1bd16638008b2d85
ioncrash <ioncrash@gmail.com>
2016-11-18 14:53:22 -0500

    build initialize
    w/sample board

---
+++ game_of_life.rb
@@ -0,0 +1,11 @@
+class Game
+  def initialize
+    @board = [
+      [nil,nil,nil,1,1],
+      [1,1,1,nil,nil],
+      [nil,1,1,nil,1],
+      [1,nil,1,nil,nil],
+      [1,nil,nil,1,1]
+    ]
+  end
+end
